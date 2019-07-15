70b057930698185efcf26697932c60cd2cc2f1bf
Charles Beach <charlesbeach@Charless-MacBook-Pro.local>
2019-07-15 18:09:43 -0400

    change code to work with eslint, adjust size of one of the walls, apply sprites to some of the objects, and add another jewel

--- game/game.js
+++ game/game.js
@@ -1,279 +1,283 @@
-/* eslint-disable */
-
-const wallGridArr = []
-const hazardGridArr = []
-const treasureGridArr = []
-const playerPos = {x: 0, y: 0}
+const wallGridArr = [];
+const hazardGridArr = [];
+const treasureGridArr = [];
+const playerPos = { x: 0, y: 0 };
 const player = document.getElementById('hero');
-const keys = [87, 38, 65, 37, 83, 40, 68, 39]
-const walls = document.querySelectorAll('.wall')
-const hazards = document.querySelectorAll('.hazard')
-const treasure = document.getElementById('treasure')
-let playerGone = false
-let treasureGone = false
+const keys = [87, 38, 65, 37, 83, 40, 68, 39];
+const walls = document.querySelectorAll('.wall');
+const hazards = document.querySelectorAll('.hazard');
+const treasures = document.querySelectorAll('.treasure');
+const mainTreasure = document.getElementById('main');
+const secretTreasure = document.getElementById('secret');
+let playerGone = false;
 
-walls[0].style.gridColumnStart = 1
-walls[0].style.gridColumnEnd = 3
-walls[0].style.gridRowStart = 2
-walls[0].style.gridRowEnd = 3
+walls[0].style.gridColumnStart = 1;
+walls[0].style.gridColumnEnd = 3;
+walls[0].style.gridRowStart = 2;
+walls[0].style.gridRowEnd = 3;
 
-walls[1].style.gridColumnStart = 5
-walls[1].style.gridColumnEnd = 6
-walls[1].style.gridRowStart = 1
-walls[1].style.gridRowEnd = 5
+walls[1].style.gridColumnStart = 5;
+walls[1].style.gridColumnEnd = 6;
+walls[1].style.gridRowStart = 1;
+walls[1].style.gridRowEnd = 5;
 
-walls[2].style.gridColumnStart = 2
-walls[2].style.gridColumnEnd = 3
-walls[2].style.gridRowStart = 2
-walls[2].style.gridRowEnd = 5
+walls[2].style.gridColumnStart = 2;
+walls[2].style.gridColumnEnd = 3;
+walls[2].style.gridRowStart = 3;
+walls[2].style.gridRowEnd = 5;
 
-walls[3].style.gridColumnStart = 2
-walls[3].style.gridColumnEnd = 3
-walls[3].style.gridRowStart = 6
-walls[3].style.gridRowEnd = 8
+walls[3].style.gridColumnStart = 2;
+walls[3].style.gridColumnEnd = 3;
+walls[3].style.gridRowStart = 6;
+walls[3].style.gridRowEnd = 8;
 
-walls[4].style.gridColumnStart = 5
-walls[4].style.gridColumnEnd = 6
-walls[4].style.gridRowStart = 6
-walls[4].style.gridRowEnd = 8
+walls[4].style.gridColumnStart = 5;
+walls[4].style.gridColumnEnd = 6;
+walls[4].style.gridRowStart = 6;
+walls[4].style.gridRowEnd = 8;
 
-walls[5].style.gridColumnStart = 8
-walls[5].style.gridColumnEnd = 9
-walls[5].style.gridRowStart = 1
-walls[5].style.gridRowEnd = 7
+walls[5].style.gridColumnStart = 8;
+walls[5].style.gridColumnEnd = 9;
+walls[5].style.gridRowStart = 2;
+walls[5].style.gridRowEnd = 7;
 
-walls[6].style.gridColumnStart = 11
-walls[6].style.gridColumnEnd = 12
-walls[6].style.gridRowStart = 1
-walls[6].style.gridRowEnd = 3
+walls[6].style.gridColumnStart = 11;
+walls[6].style.gridColumnEnd = 12;
+walls[6].style.gridRowStart = 1;
+walls[6].style.gridRowEnd = 3;
 
-walls[7].style.gridColumnStart = 11
-walls[7].style.gridColumnEnd = 13
-walls[7].style.gridRowStart = 5
-walls[7].style.gridRowEnd = 8
+walls[7].style.gridColumnStart = 11;
+walls[7].style.gridColumnEnd = 13;
+walls[7].style.gridRowStart = 5;
+walls[7].style.gridRowEnd = 8;
 
-walls[8].style.gridColumnStart = 6
-walls[8].style.gridColumnEnd = 8
-walls[8].style.gridRowStart = 3
-walls[8].style.gridRowEnd = 4
+walls[8].style.gridColumnStart = 6;
+walls[8].style.gridColumnEnd = 8;
+walls[8].style.gridRowStart = 3;
+walls[8].style.gridRowEnd = 4;
 
-hazards[0].style.gridColumnStart = 1
-hazards[0].style.gridColumnEnd = 2
-hazards[0].style.gridRowStart = 3
-hazards[0].style.gridRowEnd = 8
+hazards[0].style.gridColumnStart = 1;
+hazards[0].style.gridColumnEnd = 2;
+hazards[0].style.gridRowStart = 3;
+hazards[0].style.gridRowEnd = 8;
 
-hazards[1].style.gridColumnStart = 3
-hazards[1].style.gridColumnEnd = 4
-hazards[1].style.gridRowStart = 2
-hazards[1].style.gridRowEnd = 4
+hazards[1].style.gridColumnStart = 3;
+hazards[1].style.gridColumnEnd = 4;
+hazards[1].style.gridRowStart = 2;
+hazards[1].style.gridRowEnd = 4;
 
-hazards[2].style.gridColumnStart = 1
-hazards[2].style.gridColumnEnd = 3
-hazards[2].style.gridRowStart = 5
-hazards[2].style.gridRowEnd = 6
+hazards[2].style.gridColumnStart = 2;
+hazards[2].style.gridColumnEnd = 3;
+hazards[2].style.gridRowStart = 5;
+hazards[2].style.gridRowEnd = 6;
 
-hazards[3].style.gridColumnStart = 6
-hazards[3].style.gridColumnEnd = 7
-hazards[3].style.gridRowStart = 6
-hazards[3].style.gridRowEnd = 8
+hazards[3].style.gridColumnStart = 6;
+hazards[3].style.gridColumnEnd = 7;
+hazards[3].style.gridRowStart = 6;
+hazards[3].style.gridRowEnd = 8;
 
-hazards[4].style.gridColumnStart = 11
-hazards[4].style.gridColumnEnd = 12
-hazards[4].style.gridRowStart = 4
-hazards[4].style.gridRowEnd = 5
+hazards[4].style.gridColumnStart = 11;
+hazards[4].style.gridColumnEnd = 12;
+hazards[4].style.gridRowStart = 4;
+hazards[4].style.gridRowEnd = 5;
 
-hazards[5].style.gridColumnStart = 10
-hazards[5].style.gridColumnEnd = 11
-hazards[5].style.gridRowStart = 5
-hazards[5].style.gridRowEnd = 8
+hazards[5].style.gridColumnStart = 10;
+hazards[5].style.gridColumnEnd = 11;
+hazards[5].style.gridRowStart = 6;
+hazards[5].style.gridRowEnd = 8;
 
-hazards[6].style.gridColumnStart = 9
-hazards[6].style.gridColumnEnd = 10
-hazards[6].style.gridRowStart = 3
-hazards[6].style.gridRowEnd = 4
+hazards[6].style.gridColumnStart = 9;
+hazards[6].style.gridColumnEnd = 10;
+hazards[6].style.gridRowStart = 4;
+hazards[6].style.gridRowEnd = 5;
 
-treasure.style.gridColumnStart = 12
-treasure.style.gridColumnEnd = 13
-treasure.style.gridRowStart = 2
-treasure.style.gridRowEnd = 3
+hazards[7].style.gridColumnStart = 10;
+hazards[7].style.gridColumnEnd = 11;
+hazards[7].style.gridRowStart = 1;
+hazards[7].style.gridRowEnd = 3;
 
-function getHazardMargin() {
-    for(let i = 0; i < hazards.length; i++) {
-        //let hazardsStyle = window.getComputedStyle(hazards[i])
-        console.log(hazards[i].offsetTop)
-    }
-}
-//setInterval(getHazardMargin, 100)
+hazards[8].style.gridColumnStart = 6;
+hazards[8].style.gridColumnEnd = 8;
+hazards[8].style.gridRowStart = 4;
+hazards[8].style.gridRowEnd = 5;
+
+mainTreasure.style.gridColumnStart = 12;
+mainTreasure.style.gridColumnEnd = 13;
+mainTreasure.style.gridRowStart = 2;
+mainTreasure.style.gridRowEnd = 3;
+
+secretTreasure.style.gridColumnStart = 6;
+secretTreasure.style.gridColumnEnd = 7;
+secretTreasure.style.gridRowStart = 2;
+secretTreasure.style.gridRowEnd = 3;
+
+// fakeWall.style.gridColumnStart = 8;
+// fakeWall.style.gridColumnEnd = 9;
+// fakeWall.style.gridRowStart = 1;
+// fakeWall.style.gridRowEnd = 2;
 
 function createWallGrid() {
-    for (let i = 0; i < walls.length; i++) {
-        let arrObj = {
-            x1: parseInt(walls[i].style.gridColumnStart),
-            x2: parseInt(walls[i].style.gridColumnEnd),
-            y1: parseInt(walls[i].style.gridRowStart),
-            y2: parseInt(walls[i].style.gridRowEnd),
-        }
-        wallGridArr.push(arrObj)
-    }
-    return wallGridArr
+  for (let i = 0; i < walls.length; i += 1) {
+    const arrObj = {
+      x1: parseInt(walls[i].style.gridColumnStart, 10),
+      x2: parseInt(walls[i].style.gridColumnEnd, 10),
+      y1: parseInt(walls[i].style.gridRowStart, 10),
+      y2: parseInt(walls[i].style.gridRowEnd, 10),
+    };
+    wallGridArr.push(arrObj);
+  }
+  return wallGridArr;
 }
 
 function createTreasureGrid() {
-    let arrObj = {
-        x1: parseInt(treasure.style.gridColumnStart),
-        x2: parseInt(treasure.style.gridColumnEnd),
-        y1: parseInt(treasure.style.gridRowStart),
-        y2: parseInt(treasure.style.gridRowEnd)
-    }
-    treasureGridArr.push(arrObj)
-    return treasureGridArr
+  for (let i = 0; i < treasures.length; i += 1) {
+    const arrObj = {
+      x1: parseInt(treasures.style.gridColumnStart, 10),
+      x2: parseInt(treasures.style.gridColumnEnd, 10),
+      y1: parseInt(treasures.style.gridRowStart, 10),
+      y2: parseInt(treasures.style.gridRowEnd, 10),
+    };
+    treasureGridArr.push(arrObj);
+  }
+  console.log(treasureGridArr);
+  return treasureGridArr;
 }
 
 function createHazardGrid() {
-    for (let i = 0; i < hazards.length; i++) {
-        let arrObj = {
-            x: parseInt(hazards[i].getBoundingClientRect),
-            y: parseInt(hazards[i].getBoundingClientRect),
-        }
-        hazardGridArr.push(arrObj)
-    }
-    return hazardGridArr
+  for (let i = 0; i < hazards.length; i += 1) {
+    const arrObj = {
+      x1: parseInt(hazards[i].style.gridColumnStart, 10),
+      x2: parseInt(hazards[i].style.gridColumnEnd, 10),
+      y1: parseInt(hazards[i].style.gridRowStart, 10),
+      y2: parseInt(hazards[i].style.gridRowEnd, 10),
+    };
+    hazardGridArr.push(arrObj);
+  }
+  return hazardGridArr;
 }
 
 function makePlayerGridCoordinates(x, y) {
-    let pos = {}
-    pos.x1 = x + 1
-    pos.x2 = x + 2
-    pos.y1 = y + 1
-    pos.y2 = y + 2
-    return pos
+  const pos = {};
+  pos.x1 = x + 1;
+  pos.x2 = x + 2;
+  pos.y1 = y + 1;
+  pos.y2 = y + 2;
+  return pos;
 }
 
-createWallGrid()
-createHazardGrid()
-createTreasureGrid()
+createWallGrid();
+createHazardGrid();
+createTreasureGrid();
 
-const isInGrid = function(x, y) {
-    if (x < 0 || y < 0 || x > 11 || y > 6) {
-        return false
+const isInGrid = (x, y) => {
+  if (x < 0 || y < 0 || x > 11 || y > 6) {
+    return false;
+  }
+  return true;
+};
+
+const isBlockInWay = (pos) => {
+  for (let i = 0; i < wallGridArr.length; i += 1) {
+    if ((pos.x1 >= wallGridArr[i].x1 && pos.x2 <= wallGridArr[i].x2)
+    && (pos.y1 >= wallGridArr[i].y1 && pos.y2 <= wallGridArr[i].y2)) {
+      return true;
     }
-    return true
-}
+  }
+  return false;
+};
 
-const isBlockInWay = function(pos) {
-    for (let i = 0; i < wallGridArr.length; i++) {
-        if ((pos.x1 >= wallGridArr[i].x1 && pos.x2 <= wallGridArr[i].x2) && (pos.y1 >= wallGridArr[i].y1 && pos.y2 <= wallGridArr[i].y2)) {
-            return true
-        }
+const isHazardInWay = (pos) => {
+  for (let i = 0; i < hazardGridArr.length; i += 1) {
+    if ((pos.x1 >= hazardGridArr[i].x1 && pos.x2 <= hazardGridArr[i].x2)
+    && (pos.y1 >= hazardGridArr[i].y1 && pos.y2 <= hazardGridArr[i].y2)) {
+      return true;
     }
-    return false
-}
+  }
+  return false;
+};
 
-const isHazardInWay = function(pos) {
-    console.log('hazard')
-    for (let j = 0; j < hazardGridArr.length; j++) {
-        if ((pos.x1 >= hazardGridArr[j].x1 && pos.x2 <= hazardGridArr[j].x2) && (pos.y1 >= hazardGridArr[j].y1 && pos.y2 <= hazardGridArr[j].y2)) {
-            return true
-        }
+const isTreasureInWay = (pos) => {
+  for (let i = 0; i < treasureGridArr.length; i += 1) {
+    if ((pos.x1 === treasureGridArr[i].x1 && pos.x2 === treasureGridArr[i].x2)
+    && (pos.y1 === treasureGridArr[i].y1 && pos.y2 === treasureGridArr[i].y2)) {
+      return true;
     }
-    return false
-}
+  }
+  return false;
+};
 
-const isTreasureInWay = function(pos) {
-    for (let k = 0; k < treasureGridArr.length; k++) {
-        if ((pos.x1 === treasureGridArr[k].x1 && pos.x2 === treasureGridArr[k].x2) && (pos.y1 === treasureGridArr[k].y1 && pos.y2 === treasureGridArr[k].y2)) {
-            return true
-        }
-    }
-    return false
-}
+const removeTreasure = () => {
+  treasure.remove();
+};
 
-const removeTreasure = function() {
-    treasure.remove()
-    treasureGone = true
-}
+const removePlayer = () => {
+  player.remove();
+  playerGone = true;
+};
 
-const removePlayer = function() {
-    player.remove()
-    playerGone = true
-}
+const isAbleToMove = (x, y) => {
+  if (!isInGrid(x, y) || isBlockInWay(makePlayerGridCoordinates(x, y))) {
+    return false;
+  }
+  return true;
+};
 
-const isAbleToMove = function(x, y) {
-    if (!isInGrid(x, y) || isBlockInWay(makePlayerGridCoordinates(x, y))) {
-        return false
-    }
-    return true
-}
-
-const moveThePlayer = function(x, y) {
-    player.style.left = (playerPos.x * 100) + 'px';
-    player.style.top = (playerPos.y * 100) + 'px';
-    if (isTreasureInWay(makePlayerGridCoordinates(x, y))) {
-        removePlayer()
-        removeTreasure()
-        alert("Congratulations! You found the treasure!")
-    }
-    if (isHazardInWay(makePlayerGridCoordinates(x, y))) {
-        alert("GAME OVER")
-        removePlayer()
-    }
+function moveThePlayer(x, y) {
+  player.style.left = `${playerPos.x * 100}px`;
+  player.style.top = `${playerPos.y * 100}px`;
+  if (isTreasureInWay(makePlayerGridCoordinates(x, y))) {
+    removePlayer();
+    removeTreasure();
+    alert('Congratulations! You found the treasure!');
+  }
+  if (isHazardInWay(makePlayerGridCoordinates(x, y))) {
+    alert('GAME OVER');
+    removePlayer();
+  }
 }
 
 function goUp() {
-    if(isAbleToMove(playerPos.x, playerPos.y - 1)) {
-        playerPos.y -= 1;
-        moveThePlayer(playerPos.x, playerPos.y);
-    }
+  if (isAbleToMove(playerPos.x, playerPos.y - 1)) {
+    playerPos.y -= 1;
+    moveThePlayer(playerPos.x, playerPos.y);
+  }
 }
 
 function goLeft() {
-    if(isAbleToMove(playerPos.x - 1, playerPos.y)) {
-        playerPos.x -= 1;
-        moveThePlayer(playerPos.x, playerPos.y);
-    }
+  if (isAbleToMove(playerPos.x - 1, playerPos.y)) {
+    playerPos.x -= 1;
+    moveThePlayer(playerPos.x, playerPos.y);
+  }
 }
 
 function goDown() {
-    if(isAbleToMove(playerPos.x, playerPos.y + 1)) {
-        playerPos.y += 1;
-        moveThePlayer(playerPos.x, playerPos.y);
-    }
+  if (isAbleToMove(playerPos.x, playerPos.y + 1)) {
+    playerPos.y += 1;
+    moveThePlayer(playerPos.x, playerPos.y);
+  }
 }
 
 function goRight() {
-    if(isAbleToMove(playerPos.x + 1, playerPos.y)) {
-        playerPos.x += 1;
-        moveThePlayer(playerPos.x, playerPos.y);
-    }
+  if (isAbleToMove(playerPos.x + 1, playerPos.y)) {
+    playerPos.x += 1;
+    moveThePlayer(playerPos.x, playerPos.y);
+  }
 }
 
-document.body.addEventListener('keydown', function(evt) {
-    const keyCode = evt.keyCode;
-    if (playerGone === false) {
-        if (keys.includes(keyCode)) {
-            evt.preventDefault();
-        }
-        if (keyCode === 87 || keyCode === 38) {
-            goUp()
-        }
-        else if (keyCode === 65 || keyCode === 37) {
-            goLeft()
-        }
-        else if (keyCode === 83 || keyCode === 40) {
-            goDown()
-        }
-        else if (keyCode === 68 || keyCode === 39) {
-            goRight()
-        }
+document.body.addEventListener('keydown', (evt) => {
+  const { keyCode } = evt;
+  if (playerGone === false) {
+    if (keys.includes(keyCode)) {
+      evt.preventDefault();
     }
-})
-
-// setInterval(function(){
-//     let element = document.getElementById('hazard-one');
-//     let elementCoordiantes = element.getBoundingClientRect();
-//     console.log('HAZARD_ONE:', elementCoordiantes.x, elementCoordiantes.y)
-
-//     let hero = document.getElementById('hero')
-//     let heroCoordinates = hero.getBoundingClientRect();
-//     console.log('HERO:', heroCoordinates.x, heroCoordinates.y)
-// }, 1000)
+    if (keyCode === 87 || keyCode === 38) {
+      goUp();
+    } else if (keyCode === 65 || keyCode === 37) {
+      goLeft();
+    } else if (keyCode === 83 || keyCode === 40) {
+      goDown();
+    } else if (keyCode === 68 || keyCode === 39) {
+      goRight();
+    }
+  }
+});
